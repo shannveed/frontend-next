19ec315a8f02c49672ff8ad5d68fa6865bedef7c
shannveed <153371856+shannveed@users.noreply.github.com>
2026-05-16 11:03:51 +0500

    Update backend hostname in next.config.js

--- next.config.js
+++ next.config.js
@@ -135,12 +135,12 @@ const nextConfig = {
       { protocol: 'https', hostname: 'api-hi.moviefrost.com', pathname: '/**' },
       {
         protocol: 'https',
-        hostname: 'moviefrost-backend-ten.vercel.app',
+        hostname: 'moviefrost-backend-three.vercel.app',
         pathname: '/**',
       },
       {
         protocol: 'https',
-        hostname: 'moviefrost-backend-ten.vercel.app',
+        hostname: 'moviefrost-backend-three.vercel.app',
         pathname: '/**',
       },
       {
